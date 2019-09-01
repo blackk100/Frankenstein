c0127559d02d00518efd187a00d389bd4b60ba53
blackk100 <blackk100@users.noreply.github.com>
2019-09-01 16:23:25 +0530

    Cleaned up Ping

--- @Resources/Scripts/Ping/Ping.lua
+++ @Resources/Scripts/Ping/Ping.lua
@@ -73,41 +73,6 @@ function Set2()
 	SKIN:Bang('[!WriteKeyValue Variables '..setVar..setPos..' """'..val..'""" "#@#/Scripts/Ping/Settings.inc"][!SetOption Meter'..setVar..'Set Text """'..val..'"""][!UpdateMeter Meter'..setVar..'Set][!Redraw]')
 end
 
-function Swap(n, m)
-	local varName = {'Open', 'Name', 'Address', 'ThresholdY', 'ThresholdR'}
-	for i = 1, 6 do
-		if i ~= 3 and i ~= 4 then
-			variable[varName[i]][n], variable[varName[i]][m] = variable[varName[i]][m], variable[varName[i]][n]
-		end
-		SKIN:Bang('[!WriteKeyValue Variables '..varName[i]..n..' """#'..varName[i]..m..'#""" "#@#/Scripts/Ping/Settings.inc"][!WriteKeyValue Variables '..varName[i]..m..' """#'..varName[i]..n..'#""" "#@#/Scripts/Ping/Settings.inc"][!SetVariable '..varName[i]..n..' """#'..varName[i]..m..'#"""][!SetVariable '..varName[i]..m..' """#'..varName[i]..n..'#"""]')
-	end
-	SKIN:Bang('[!SetOption MeasurePing'..n..' DestAddress "#Address'..n..'#"][!SetOption MeasurePing'..m..' DestAddress "#Address'..m..'#"][!UpdateMeasure MeasurePing'..n..'][!UpdateMeasure MeasurePing'..m..']')
-	if variable['Open'][n] ~= variable['Open'][m] then
-		flagSet = false
-		ToggleGraph(n, (variable['Open'][n] == 0 and 1 or nil))
-		ToggleGraph(m, (variable['Open'][m] == 0 and 1 or nil))
-		flagSet = true
-	end
-end
-
-function Shift(p)
-	if setPos + p >= 1 and setPos + p <= active then
-		Swap(setPos, setPos + p)
-		ShowSet()
-	end
-end
-
-function Remove()
-	if active == 1 then
-		return
-	end
-	for i = setPos, active - 1 do
-		Swap(i, i + 1)
-	end
-	SKIN:Bang('[!DisableMeasure MeasurePing'..active..'][!HideMeter MeterGraph'..active..'][!HideMeterGroup '..active..'][!WriteKeyValue Variables Active '..(active - 1)..' "#@#/Scripts/Ping/Settings.inc"][!Redraw]')
-	active = active - 1
-end
-
 function Add(n)
 	if active == 10 then
 		return
