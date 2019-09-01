e6f7783d2c41267779884b154c228020d0118666
blackk100 <blackk100@users.noreply.github.com>
2019-09-01 16:20:04 +0530

    Renamed sections; Metadata fixes
    Closes issue #2

--- @Resources/Scripts/Ping/Ping.lua
+++ @Resources/Scripts/Ping/Ping.lua
@@ -1,25 +1,25 @@
 function Initialize()
-	active, mPing, ping, variable, tint, graphH, flagSet, flagInput, setVar, setPos = 0, {}, {}, {['Open'] = {}, ['Name'] = {}, ['ThresholdY'] = {}, ['ThresholdR'] = {}}, {'00FF00', 'FFFF00', 'FF0000', 'FF0000'}, tonumber(SKIN:GetVariable('GraphH')), false, false
+	active, MeasurePing, ping, variable, tint, graphH, flagSet, flagInput, setVar, setPos = 0, {}, {}, {['Open'] = {}, ['Name'] = {}, ['ThresholdY'] = {}, ['ThresholdR'] = {}}, {'00FF00', 'FFFF00', 'FF0000', 'FF0000'}, tonumber(SKIN:GetVariable('GraphH')), false, false
 	local varName = {'Open', 'Name', 'ThresholdY', 'ThresholdR'}
 	for i = 1, 10 do
 		for j = 1, 4 do
 			variable[varName[j]][i] = tonumber(SKIN:GetVariable(varName[j]..i)) or SKIN:GetVariable(varName[j]..i)
 		end
-		mPing[i] = SKIN:GetMeasure('mPing'..i)
+		MeasurePing[i] = SKIN:GetMeasure('MeasurePing'..i)
 	end
 	Add(tonumber(SKIN:GetVariable('Active')))
 end
 
 function Update()
 	for i = 1, active do
-		local p = mPing[i]:GetValue()
+		local p = MeasurePing[i]:GetValue()
 		p = p < variable['ThresholdY'][i] and 1 or p < variable['ThresholdR'][i] and 2 or p == 30000 and 4 or 3
 		if p ~= ping[i] then
-			SKIN:Bang('[!SetOption Dot'..i..' ImageTint '..tint[p]..'][!UpdateMeter Dot'..i..']')
+			SKIN:Bang('[!SetOption MeterDot'..i..' ImageTint '..tint[p]..'][!UpdateMeter MeterDot'..i..']')
 			if ping[i] == 4 then
-				SKIN:Bang('[!SetOption Num'..i..' FontColor FFFFFF][!UpdateMeter Num'..i..']')
+				SKIN:Bang('[!SetOption MeterNumber'..i..' FontColor FFFFFF][!UpdateMeter MeterNumber'..i..']')
 			elseif p == 4 then
-				SKIN:Bang('[!SetOption Num'..i..' FontColor FF0000][!UpdateMeter Num'..i..']')
+				SKIN:Bang('[!SetOption MeterNumber'..i..' FontColor FF0000][!UpdateMeter MeterNumber'..i..']')
 			end
 			ping[i] = p
 		end
@@ -29,27 +29,27 @@ end
 
 function ToggleName(n, m)
 	if variable['Name'][n] ~= '' then
-		SKIN:Bang('[!SetOption Num'..n..' Text '..(m and '#*Name'..n..'*#' or '""')..'][!UpdateMeter Num'..n..'][!Redraw]')
+		SKIN:Bang('[!SetOption MeterNumber'..n..' Text '..(m and '#*Name'..n..'*#' or '""')..'][!UpdateMeter MeterNumber'..n..'][!Redraw]')
 	end
 end
 
 function ToggleGraph(n, m)
 	if not flagSet then
 		variable['Open'][n] = m and 0 or 1
-		SKIN:Bang('[!'..(m and 'Hide' or 'Show')..'Meter Graph'..n..'][!SetOption Num'..n..' LeftMouseUpAction '..(m and '"!CommandMeasure mScript ToggleGraph('..n..')"' or '""')..'][!UpdateMeter Num'..n..']'..(n < 10 and '[!SetOption Graph'..(n + 1)..' Y '..(m and 9 or (graphH - 8))..'r][!UpdateMeter Graph'..(n + 1)..']' or '')..'[!WriteKeyValue Variables Open'..n..' '..(m and 0 or 1)..' "#@#Settings.inc"][!HideMeter Cog][!Redraw]')
+		SKIN:Bang('[!'..(m and 'Hide' or 'Show')..'Meter MeterGraph'..n..'][!SetOption MeterNumber'..n..' LeftMouseUpAction '..(m and '"!CommandMeasure MeasureScript ToggleGraph('..n..')"' or '""')..'][!UpdateMeter MeterNumber'..n..']'..(n < 10 and '[!SetOption MeterGraph'..(n)..' Y '..(m and 9 or (graphH - 8))..'r][!UpdateMeter MeterGraph'..(n)..']' or '')..'[!WriteKeyValue Variables Open'..n..' '..(m and 0 or 1)..' "#@#/Scripts/Ping/Settings.inc"][!HideMeter MeterCog][!Redraw]')
 	end
 end
 
 function ShowCog(n)
 	if not flagSet then
-		SKIN:Bang('[!MoveMeter 0 '..(SKIN:GetMeter('Graph'..n):GetY())..' Cog][!ShowMeter Cog][!Redraw]')
+		SKIN:Bang('[!MoveMeter 0 '..(SKIN:GetMeter('MeterGraph'..n):GetY())..' MeterCog][!ShowMeter MeterCog][!Redraw]')
 		setPos = n
 	end
 end
 
 function ShowSet()
 	flagSet = true
-	SKIN:Bang('[!MoveMeter (#GraphW#/4) '..(SKIN:GetMeter('Graph'..setPos):GetY())..' ShiftUp][!SetOption NameSet Text #*Name'..setPos..'*#][!SetOption AddressSet Text #*Address'..setPos..'*#][!SetOption ThresholdYSet Text #*ThresholdY'..setPos..'*#][!SetOption ThresholdRSet Text #*ThresholdR'..setPos..'*#][!UpdateMeterGroup Set][!ShowMeterGroup Set][!Redraw]')
+	SKIN:Bang('[!SetOption MeterNameSet Text #*Name'..setPos..'*#][!SetOption MeterAddressSet Text #*Address'..setPos..'*#][!SetOption MeterThresholdYSet Text #*ThresholdY'..setPos..'*#][!SetOption MeterThresholdRSet Text #*ThresholdR'..setPos..'*#][!UpdateMeterGroup Set][!ShowMeterGroup Set][!Redraw]')
 end
 
 function HideSet()
@@ -61,16 +61,16 @@ end
 
 function Set1(var, y, command)
 	setVar, flagInput = var, true
-	SKIN:Bang('[!SetVariable Var '..var..setPos..'][!SetVariable VarValue """#'..var..setPos..'#"""][!SetVariable Y '..(SKIN:GetMeter('Graph'..setPos):GetY() + y)..'][!Update][!CommandMeasure mInput "ExecuteBatch '..command..'"]')
+	SKIN:Bang('[!SetVariable Var '..var..setPos..'][!SetVariable VarValue """#'..var..setPos..'#"""][!SetVariable Y '..(SKIN:GetMeter('MeterGraph'..setPos):GetY() + y)..'][!Update][!CommandMeasure MeasureInput "ExecuteBatch '..command..'"]')
 end
 
 function Set2()
 	flagInput = false
 	local val = SKIN:GetVariable(SKIN:GetVariable('Var'))
 	if setVar == 'Address' then
-		SKIN:Bang('[!SetOption mPing'..setPos..' DestAddress "'..val..'"][!UpdateMeasure mPing'..setPos..']')
+		SKIN:Bang('[!SetOption MeasurePing'..setPos..' DestAddress "'..val..'"][!UpdateMeasure MeasurePing'..setPos..']')
 	end
-	SKIN:Bang('[!WriteKeyValue Variables '..setVar..setPos..' """'..val..'""" "#@#Settings.inc"][!SetOption '..setVar..'Set Text """'..val..'"""][!UpdateMeter '..setVar..'Set][!Redraw]')
+	SKIN:Bang('[!WriteKeyValue Variables '..setVar..setPos..' """'..val..'""" "#@#/Scripts/Ping/Settings.inc"][!SetOption Meter'..setVar..'Set Text """'..val..'"""][!UpdateMeter Meter'..setVar..'Set][!Redraw]')
 end
 
 function Swap(n, m)
@@ -79,9 +79,9 @@ function Swap(n, m)
 		if i ~= 3 and i ~= 4 then
 			variable[varName[i]][n], variable[varName[i]][m] = variable[varName[i]][m], variable[varName[i]][n]
 		end
-		SKIN:Bang('[!WriteKeyValue Variables '..varName[i]..n..' """#'..varName[i]..m..'#""" "#@#Settings.inc"][!WriteKeyValue Variables '..varName[i]..m..' """#'..varName[i]..n..'#""" "#@#Settings.inc"][!SetVariable '..varName[i]..n..' """#'..varName[i]..m..'#"""][!SetVariable '..varName[i]..m..' """#'..varName[i]..n..'#"""]')
+		SKIN:Bang('[!WriteKeyValue Variables '..varName[i]..n..' """#'..varName[i]..m..'#""" "#@#/Scripts/Ping/Settings.inc"][!WriteKeyValue Variables '..varName[i]..m..' """#'..varName[i]..n..'#""" "#@#/Scripts/Ping/Settings.inc"][!SetVariable '..varName[i]..n..' """#'..varName[i]..m..'#"""][!SetVariable '..varName[i]..m..' """#'..varName[i]..n..'#"""]')
 	end
-	SKIN:Bang('[!SetOption mPing'..n..' DestAddress "#Address'..n..'#"][!SetOption mPing'..m..' DestAddress "#Address'..m..'#"][!UpdateMeasure mPing'..n..'][!UpdateMeasure mPing'..m..']')
+	SKIN:Bang('[!SetOption MeasurePing'..n..' DestAddress "#Address'..n..'#"][!SetOption MeasurePing'..m..' DestAddress "#Address'..m..'#"][!UpdateMeasure MeasurePing'..n..'][!UpdateMeasure MeasurePing'..m..']')
 	if variable['Open'][n] ~= variable['Open'][m] then
 		flagSet = false
 		ToggleGraph(n, (variable['Open'][n] == 0 and 1 or nil))
@@ -104,7 +104,7 @@ function Remove()
 	for i = setPos, active - 1 do
 		Swap(i, i + 1)
 	end
-	SKIN:Bang('[!DisableMeasure mPing'..active..'][!HideMeter Graph'..active..'][!HideMeterGroup '..active..'][!WriteKeyValue Variables Active '..(active - 1)..' "#@#Settings.inc"][!Redraw]')
+	SKIN:Bang('[!DisableMeasure MeasurePing'..active..'][!HideMeter MeterGraph'..active..'][!HideMeterGroup '..active..'][!WriteKeyValue Variables Active '..(active - 1)..' "#@#/Scripts/Ping/Settings.inc"][!Redraw]')
 	active = active - 1
 end
 
@@ -114,10 +114,10 @@ function Add(n)
 	end
 	for i = 1, n do
 		active = active + 1
-		SKIN:Bang('[!EnableMeasure mPing'..active..'][!ShowMeterGroup '..active..']')
+		SKIN:Bang('[!EnableMeasure MeasurePing'..active..'][!ShowMeterGroup '..active..']')
 		if variable['Open'][active] == 1 then
 			ToggleGraph(active)
 		end
 	end
-	SKIN:Bang('[!WriteKeyValue Variables Active '..active..' "#@#Settings.inc"][!Redraw]')
+	SKIN:Bang('[!WriteKeyValue Variables Active '..active..' "#@#/Scripts/Ping/Settings.inc"][!Redraw]')
 end
